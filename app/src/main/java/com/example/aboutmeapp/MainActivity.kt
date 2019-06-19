fc40afd7279e03a0a127111e1433df77a5003325
Bramdont <bramdont@gmail.com>
2019-06-19 14:19:40 -0400

    Using DataBinding to make the About Me App do the same as before, but in a more efficient way

---
+++ app/src/main/java/com/example/aboutmeapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.aboutmeapp
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.aboutmeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneBtn.setOnClickListener { addNickname(it) }
+
+    }
+
+    private fun addNickname(view: View){
+
+        binding.apply {
+
+            nicknameTxt.text = nickNameEdtxt.text
+            nickNameEdtxt.visibility = View.GONE
+            doneBtn.visibility = View.GONE
+            nicknameTxt.visibility = View.VISIBLE
+        }
+
+        //Code to hide the keyboard after we're done
+        val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
